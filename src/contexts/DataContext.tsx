d3fbe76a89c0599d8b171de7bf9620b19eb96c36
alexM421 <alexandremillet421@gmail.com>
2025-08-08 19:34:58 +0200

    Added dataContext

---
+++ src/contexts/DataContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useContext, useState } from "react";
+import  SampleData  from "../../data.json"
+
+
+type Data = {
+    balance: {
+        current: number,
+        income: number,
+        expenses: number,
+    },
+    transactions: {
+            avatar: string,
+            name: string,
+            category: string,
+            date: string,
+            amount: number,
+            recurring: boolean,
+    }[],
+    budgets: 
+        {
+            category: string,
+            maximum: number,
+            theme: string,
+        }[],
+    pots: 
+        {
+            name: string,
+            target: number,
+            total: number,
+            theme: string,
+        }[]
+}
+
+type DataContextValue = {
+    data: Data;
+    setData: React.Dispatch<React.SetStateAction<Data>>
+}
+
+const DataContext = createContext<DataContextValue | undefined>(undefined)
+
+type DataProviderProps = {
+    children: React.ReactNode
+}
+
+export function DataProvider ({ children }: DataProviderProps) {
+
+    const [data, setData] = useState<Data>(SampleData)
+
+    const value = {
+        data: data,
+        setData: setData
+    }
+
+    return(
+        <DataContext.Provider value={value}>
+            { children }
+        </DataContext.Provider>
+    )
+}
+
+export function useDataContext () {
+
+    const context = useContext(DataContext)
+
+    if(!context){
+        throw new Error("DataContext is not defined.")
+    }
+
+    return context
+    
+}
