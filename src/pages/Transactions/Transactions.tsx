a0a28b19c5f25f589b9a94b8ec2501ca272bd6e7
alexM421 <alexandremillet421@gmail.com>
2025-08-11 15:44:19 +0200

    Transactions init

---
+++ src/pages/Transactions/Transactions.tsx
@@ -0,0 +1,12 @@
+//CSS
+import styles from "./Transactions.module.css"
+
+export default function Transactions () {
+
+
+    return(
+        <div className={styles.transactions}>
+            
+        </div>
+    )
+}
