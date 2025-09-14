385b2d700d69c65d9f691b6fbba48940d4e33ed2
alexM421 <alexandremillet421@gmail.com>
2025-09-14 16:39:13 +0200

    Fixed recurring bills ui overview issue

--- src/pages/Overview/OverviewRecurringBillsItem.tsx
+++ src/pages/Overview/OverviewRecurringBillsItem.tsx
@@ -1,5 +1,8 @@
 //CSS
 import styles from "./Overview.module.css"
+//context
+import { useDataContext } from "../../contexts/DataContext"
+import formatNumber from "../../utils/formatNumber"
 
 type OverviewRecurringBillsItemProps = {
     theme: string,
@@ -9,13 +12,15 @@ type OverviewRecurringBillsItemProps = {
 
 export default function OverviewRecurringBillsItem ({theme, title, amount}: OverviewRecurringBillsItemProps) {
 
+    const { data: { personnalSettings: { preferredCurrency }}} = useDataContext()
+
     return(
         <div 
         className={styles["overview-recurring-bills-item"]}
         style={{boxShadow: `-4px 0px 0px ${theme}`}}
         >
             <p className="text-preset-4">{title}</p>
-            <h2 className="text-preset-4-bold">${amount.toFixed(2)}</h2>
+            <h2 className="text-preset-4-bold">{formatNumber(amount, preferredCurrency, false)}</h2>
         </div>
     )
 }
