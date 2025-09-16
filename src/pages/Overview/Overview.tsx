3c04c54128cc7832fed2e79e68e1f37c1bf09866
alexM421 <alexandremillet421@gmail.com>
2025-09-16 14:37:44 +0200

    Fixed overview stats and created a component for it

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -12,12 +12,9 @@ import OverviewRecurringBills from "./OverviewRecurringBills"
 import OverviewTransactions from "./OverviewTransactions"
 import ModalLayout from "../../modals/ModalLayout/ModalLayout"
 import SettingsModal from "../../modals/SettingsModal/SettingsModal"
+import OverviewStats from "./OverviewStats"
 
 export default function Overview () {
-
-    const { current, income, expenses } = useDataContext().data.balance
-
-    const { data: { personnalSettings: { preferredCurrency }}} = useDataContext()
      
     const [settingsDisplay, setSettingsDisplay] = useState(false)
 
@@ -28,20 +25,7 @@ export default function Overview () {
                     <h1 className="text-preset-1">Overview</h1>
                     <Button onClick={() => setSettingsDisplay(true)}><IconSettings/></Button>
                 </div> 
-                <div className={styles["overview-stats"]}>
-                    <div>
-                        <p className="text-preset-4">Current Balance</p>
-                        <h2 className="text-preset-1">{formatNumber(current, preferredCurrency, false)}</h2>
-                    </div>
-                    <div>
-                        <p className="text-preset-4">Income</p>
-                        <h2 className="text-preset-1">{formatNumber(income, preferredCurrency, false)}</h2>
-                    </div>
-                    <div>
-                        <p className="text-preset-4">Expenses</p>
-                        <h2 className="text-preset-1">{formatNumber(expenses, preferredCurrency, false)}</h2>
-                    </div>
-                </div>
+                <OverviewStats/>
                 <div className={styles["overview-main"]}>
                     <div>
                         <OverviewPots/>
