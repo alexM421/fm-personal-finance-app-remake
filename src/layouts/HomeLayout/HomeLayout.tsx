e98990c7fde1169512ca39fe343613358a7b9a72
alexM421 <alexandremillet421@gmail.com>
2025-10-03 11:27:01 +0200

    Added redirects

--- src/layouts/HomeLayout/HomeLayout.tsx
+++ src/layouts/HomeLayout/HomeLayout.tsx
@@ -2,24 +2,30 @@
 import styles from "./HomeLayout.module.css"
 //layouts
 import Navbar from "../Navbar/Navbar"
+import MobileNavbar from "../Navbar/MobileNavbar/MobileNavbar"
 //React
-import { Navigate, Outlet } from "react-router"
+import { Navigate, Outlet, useLocation } from "react-router"
 //contexts
 import { useAuthContext } from "../../contexts/AuthContext"
 //hooks
 import useSyncBill from "../../hooks/useSyncBill"
 import useMobileListener from "../../hooks/useMobileListener"
-import MobileNavbar from "../Navbar/MobileNavbar/MobileNavbar"
 
 
 export default function HomeLayout () {
-
+    
+    const { pathname } = useLocation()
     const { loading, auth } = useAuthContext()
 
     if(!auth && !loading){
         console.log("redirected to login")
         return(
             <Navigate to="/auth/login"/>
+        )
+    }
+    if(pathname==="/"){
+        return(
+            <Navigate to="/overview"/>
         )
     }
 
